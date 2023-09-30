9332aaada2c747097bbf220b21e5d59ed77bb4a7
RexshackGaming <rexshackuk@gmail.com>
2023-09-30 13:37:44 +0100

    version checker

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,37 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 local ResetStress = false
 
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/Rexshack-RedM/rsg-hud/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'Currently unable to run a version check.')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are running the latest version.')
+        else
+            versionCheckPrint('error', ('You are currently running an outdated version, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
 RSGCore.Commands.Add('cash', 'Check Cash Balance', {}, false, function(source, args)
     local Player = RSGCore.Functions.GetPlayer(source)
     local cashamount = Player.PlayerData.money.cash
@@ -109,3 +140,8 @@ RSGCore.Functions.CreateCallback('hud:server:getTelegramsAmount', function(sourc
         cb(0)
     end
 end)
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
